bdebb0cac2347f654fa0ef0ab1f049be1e182a7d
Howe <LiaoHowe@qq.com>
2021-11-10 22:57:54 +0800

    完成整个轮播图部分、左右切换、鼠标移入移除

--- TextItem/js/index.js
+++ TextItem/js/index.js
@@ -1,33 +1,57 @@
 // 轮播图
 {
-    let images = document.querySelectorAll(".img_box>li"); // 获取轮播图片
-    let dotBox = document.querySelectorAll(".dot_box>li"); // 获取播放点
-    let current = 0; // 当前播放位置
-    let Timer; // 定时器
-    autoplay();
+    let carousel    = document.querySelector(".left.banner");
+    let images      = document.querySelectorAll(".img_box>li");    // 获取轮播图片
+    let dotBox      = document.querySelectorAll(".dot_box>li");    // 获取播放点
+    let arrow       = document.querySelectorAll(".arrow>li");    // 获取播放点
+    let current     = 0;    // 当前播放位置
+    let Timer       = null; // 定时器
 
+    // 自动轮播
     function autoplay() {
-        timer = setInterval(() => {
-            // 删除兄弟节点 class
-            images.forEach(item=>{
-                item.classList.remove("current");
-            });
-            dotBox.forEach(item=>{
-                item.classList.remove("current");
-            });
-            // 添加当前类
-            images[current].classList.add("current");
-            dotBox[current].classList.add("current");
+        Timer = setInterval(() => {
+            SwitchImage(current);
             // 判断是否轮播到最后一张
-            if(current == images.length-1){current=0}else{current++;}
+            if(current == images.length-1){current=0}else{current++;};
         }, 1500);
     }
+
+    // 图片切换
+    function SwitchImage(current){
+        // 删除兄弟节点 class
+        images.forEach(item=>{
+            item.classList.remove("current");
+        });
+        dotBox.forEach(item=>{
+            item.classList.remove("current");
+        });
+        // 添加当前类
+        images[current].classList.add("current");
+        dotBox[current].classList.add("current");
+    };
+    autoplay();
+    // 鼠标移入 移出事件
+    carousel.addEventListener("mouseover",()=>{
+        clearInterval(Timer);
+    });
+    carousel.addEventListener("mouseout",()=>{
+        autoplay();
+    });
+
+    // 左右按钮点击事件
+    arrow[0].addEventListener("click",()=>{
+        if (current == 0) {current = images.length - 1;}else{current--;};
+        SwitchImage(current);
+    })
+    arrow[1].addEventListener("click",()=>{
+        if (current == images.length - 1) {current = 0;}else{current++;};
+        SwitchImage(current);
+    })
 }
 
 // 曲线图
 {
     let graph = echarts.init(document.getElementById('graph'));
-
     // 指定图表的配置项和数据
     let option = {
         title: {
@@ -49,7 +73,6 @@
             areaStyle: {}
         }]
     };
-
     // 使用刚指定的配置项和数据显示图表。
     graph.setOption(option);
 }
