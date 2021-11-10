398eb3f3985eabd2bc3006e6fc177f9e57421e37
liaohowe@qq.com <liaohowe@qq.com>
2021-11-10 17:11:35 +0800

    完成整个页面布局，以及echarts示例

---
+++ TextItem/js/index.js
@@ -0,0 +1,87 @@
+// 曲线图
+{
+    let graph = echarts.init(document.getElementById('graph'));
+
+    // 指定图表的配置项和数据
+    let option = {
+        title: {
+            text: '曲线数据图展示',
+            left: 'center'
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        color: "#337af1",
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: 'line',
+            areaStyle: {}
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    graph.setOption(option);
+}
+
+// 饼状图
+{
+    let pieChart = echarts.init(document.getElementById('pieChart'));
+    let option = {
+        title: {
+            text: '饼状图数据展示',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        series: [{
+            name: 'Access From',
+            type: 'pie',
+            radius: '50%',
+            data: [
+                { value: 1048, name: 'Search Engine' },
+                { value: 735, name: 'Direct' },
+                { value: 580, name: 'Email' },
+                { value: 484, name: 'Union Ads' },
+                { value: 300, name: 'Video Ads' }
+            ],
+            emphasis: {
+                itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }]
+    };
+    pieChart.setOption(option);
+}
+
+// 柱状图
+{
+    let barGraph = echarts.init(document.getElementById('barGraph'));
+    let option = {
+        title: {
+            text: '柱状图数据展示',
+            left: 'center'
+        },
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar'
+        }]
+    };
+
+    barGraph.setOption(option);
+}
