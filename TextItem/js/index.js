14d0d7dffb0cd636fbc7ed4057711fdde869f8a2
liaohowe@qq.com <liaohowe@qq.com>
2021-11-11 10:52:07 +0800

    Finish

--- TextItem/js/index.js
+++ TextItem/js/index.js
@@ -73,8 +73,6 @@ function ajaxGet(type) {
  *  @param { RenderingNode }    seriesData      渲染节点
  */
  function chartData(titleText,seriesType,RenderingNode,xAxisData,seriesData){
-	 console.log(seriesData)
-	 // debugger
     let option =  {
         title: {
             text: titleText,
@@ -107,7 +105,6 @@ function ajaxGet(type) {
 	parameterFailure.onreadystatechange = function (){
 		if (parameterFailure.readyState == 4 && parameterFailure.status == 200) {
 			receiveData = JSON.parse(parameterFailure.responseText);
-			console.log(receiveData.data);
 			chartData("曲线数据图展示","line",graph,receiveData.data.xAxis,receiveData.data.series);
 		}
 	}
@@ -121,42 +118,42 @@ function ajaxGet(type) {
 	parameterFailure.onreadystatechange = function (){
 		if (parameterFailure.readyState == 4 && parameterFailure.status == 200) {
 			receiveData = JSON.parse(parameterFailure.responseText);
-			console.log(receiveData.data);
+			pieChartFn(receiveData.data);	// 调用生成饼状图
 			chartData("柱状图数据展示","bar",barGraph,receiveData.data.xAxis,receiveData.data.series);
 		}
 	}
+	
+	// 饼状图
+	
 }
 
 // 饼状图
-{
-    let pieChart = echarts.init(document.getElementById('pieChart'));
-    let option = {
-        title: {
-            text: '饼状图数据展示',
-            left: 'center'
-        },
-        tooltip: {
-            trigger: 'item'
-        },
-        series: [{
-            name: 'Access From',
-            type: 'pie',
-            radius: '50%',
-            data: [
-                { value: 1048, name: 'Search Engine' },
-                { value: 735, name: 'Direct' },
-                { value: 580, name: 'Email' },
-                { value: 484, name: 'Union Ads' },
-                { value: 300, name: 'Video Ads' }
-            ],
-            emphasis: {
-                itemStyle: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
-            }
-        }]
-    };
-    pieChart.setOption(option);
-}
+function pieChartFn(receiveData){
+	let pieChart = echarts.init(document.getElementById('pieChart'));
+	let option = {
+	    title: {
+	        text: '饼状图数据展示',
+	        left: 'center'
+	    },
+	    tooltip: {
+	        trigger: 'item'
+	    },
+	    series: [{
+	        name: 'Access From',
+	        type: 'pie',
+	        radius: '50%',
+	        data: [],
+	        emphasis: {
+	            itemStyle: {
+	                shadowBlur: 10,
+	                shadowOffsetX: 0,
+	                shadowColor: 'rgba(0, 0, 0, 0.5)'
+	            }
+	        }
+	    }]
+	};
+	receiveData.series.forEach((item,index)=>{
+		option.series[0].data.push({ value:item, name:receiveData.xAxis[index]});			
+	})
+	pieChart.setOption(option);
+};
